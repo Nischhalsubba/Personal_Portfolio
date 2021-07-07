99912cf273926c2acd7121a4af7de08b41487e82
Nischhal Subba <hinischalsubba@gmail.com>
2021-07-07 17:21:18 +0545

    Version 2 of personal portfolio design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,19 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+"use strict";
 
-// Background particles
-var particles = document.getElementById("particles");
-var border = ["50%", "0%"];
-var colors = ["#FF6B6B", "#FFE66D", "#4472CA"];
+// Open & Close Aside Navigation
 
-function createParticle(event) {
-  var x = event.clientX;
-  var y = event.clientY;
-  var color = Math.floor(Math.random() * 3);
+const openNav = document.querySelector(".open-nav");
+const closeNav = document.querySelector(".close-nav");
+const asideOverlay = document.querySelector(".aside-navigation-overlay");
+const asideNav = document.querySelector(".aside-navigation");
 
-  var div = document.createElement("div");
-  div.style.position = "absolute";
-  div.style.marginLeft = x + "px";
-  div.style.marginTop = y + "px";
-  div.style.width = "10px";
-  div.style.borderTop = "5px solid transparent";
-  div.style.borderRight = "5px solid transparent";
-  div.style.borderLeft = "5px solid transparent";
-  div.style.borderBottom = "10px solid " + colors[color];
-  div.style.animation = "move 5s ease-in infinite";
-  particles.appendChild(div);
-  setTimeout(
-    function () {
-      div.remove();
-    }, 5000);
-}
+openNav.addEventListener("click", function(){
+	asideOverlay.classList.add("active");
+	asideNav.classList.add("active");
+})
 
-function getParticles() {
-  var np = document.documentElement.clientWidth / 40;
-  particles.innerHTML = "";
-  for (var i = 0; i < np; i++) {
-    var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
-    var rndw = Math.floor(Math.random() * w) + 1;
-    var rndh = Math.floor(Math.random() * h) + 1;
-    var widthpt = Math.floor(Math.random() * 8) + 5;
-    var opty = Math.floor(Math.random() * 4) + 1;
-    var anima = Math.floor(Math.random() * 12) + 8;
-    var bdr = Math.floor(Math.random() * 2);
-    var color = Math.floor(Math.random() * 3);
+closeNav.addEventListener("click", function(){
+	asideOverlay.classList.remove("active");
+	asideNav.classList.remove("active");
+})
 
-    var div = document.createElement("div");
-    div.style.position = "absolute";
-    div.style.marginLeft = rndw + "px";
-    div.style.marginTop = rndh + "px";
-    div.style.width = widthpt + "px";
-    div.style.height = widthpt + "px";
-    div.style.opacity = opty;
-    div.style.backgroundColor = colors[color];
-    div.style.borderRadius = border[bdr];
-    div.style.animation = "move " + anima + "s ease-in infinite";
-    particles.appendChild(div);
-  }
-}
-
-function main(event) {
-  getParticles();
-  particles.addEventListener("click", createParticle);
-}
-
-window.addEventListener("resize", main);
-window.addEventListener("load", main);
