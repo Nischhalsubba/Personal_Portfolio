9c98969e43ba530b90fd1b5b8dd4aad41fa7c500
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-07 10:23:07 +0545

    Restore finite build and migrate to Dart Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 "use strict";
-// Initailize Modules
 const {
     src,
     dest,
@@ -11,22 +10,19 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
-var replace = require('gulp-replace');
-const sass = require('gulp-sass');
+const replace = require('gulp-replace');
+const sass = require('gulp-sass/legacy')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-// const uglify = require('gulp-uglify');
-const terser = require ('gulp-terser');
-const imagemin = require("gulp-imagemin");
-const browsersync = require("browser-sync").create();
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
+const browsersync = require('browser-sync').create();
 
-// File path variables
 const files = {
     scssPath: './assets/sass/**/*.scss',
     jsPath: './assets/js/**/*.js',
     imgPath: './assets/Images/**'
-}
+};
 
-// sass task
 function scssTask() {
     return src(files.scssPath)
         .pipe(sourcemaps.init())
@@ -36,7 +32,6 @@ function scssTask() {
         .pipe(dest('./dest/'));
 }
 
-// JS task
 function jsTask() {
     return src(files.jsPath)
         .pipe(concat('app.js'))
@@ -44,34 +39,27 @@ function jsTask() {
         .pipe(dest('./js/'));
 }
 
-// BrowserSync
 function browserSync(done) {
     browsersync.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         },
         port: 3000
     });
     done();
 }
 
-// BrowserSync Reload
 function browserSyncReload(done) {
     browsersync.reload();
     done();
 }
 
-// Optimize Images
 function images() {
     return src(files.imgPath)
         .pipe(
             imagemin([
-                imagemin.gifsicle({
-                    interlaced: true
-                }),
-                imagemin.optipng({
-                    optimizationLevel: 5
-                }),
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
                 imagemin.svgo({
                     plugins: [{
                         removeViewBox: false,
@@ -80,30 +68,27 @@ function images() {
                 })
             ])
         )
-        .pipe(dest("./dest/Images/"));
+        .pipe(dest('./dest/Images/'));
 }
 
-// cachebusting task
 const cbString = new Date().getTime();
 
 function cacheBustTask() {
     return src(['./*.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString)) //using gulp replace plugin to prevent caching (using regex /'what we looking for' 'cb=' 'looking for any number' '\d'+/g)=> looking for nth number of digit
-        //replace('what we are looking for ','what are replacing with')
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
         .pipe(dest('.'));
 }
 
-// watch task
 function watchTasks() {
-    watch([files.scssPath, files.jsPath,files.imgPath],
-        parallel(scssTask, jsTask,images,images)
+    watch([files.scssPath, files.jsPath, files.imgPath],
+        parallel(scssTask, jsTask, images)
     );
 }
 
-// Default task
-exports.default = series(
-    parallel(scssTask, jsTask),
-    cacheBustTask,
-    watchTasks,
-    images,
-)
+const build = series(
+    parallel(scssTask, jsTask, images),
+    cacheBustTask
+);
+
+exports.build = build;
+exports.default = series(build, parallel(browserSync, watchTasks));
